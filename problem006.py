b7a80ce2305951d4ae083b6b3941a4afbb467f05
Afreen Aliya <afreenjuly@gmail.com>
2018-06-15 13:47:33 -0400

    Add files via upload

---
+++ problem006.py
@@ -0,0 +1,14 @@
+sum_of_square = 0
+summation = 0
+square_of_sum = 0
+
+for i in range(1, 101):
+	sum_of_squares += i*i
+
+
+for i in range(1, 101):
+	summamtion +=i
+
+square_of_sum += summamtion*summation
+
+print(square_of_sum - sum_of_square)
